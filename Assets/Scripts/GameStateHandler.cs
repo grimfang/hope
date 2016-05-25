5945ce6fae909576c25a2c3ca9c852f10d45a533
fireclawthefox <fireclawthefox@gmail.com>
2016-05-25 21:00:51 +0200

    Added menu and game state handler

---
+++ Assets/Scripts/GameStateHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateHandler : MonoBehaviour {
+
+	public static GameStateHandler instance = null;
+	public GameObject menuManager;
+
+	private int gameState;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+		InitMenu ();
+	}
+
+	// Start Menu
+	void InitMenu()
+	{
+
+	}
+
+	// Start Game
+	void InitGame(int gm)
+	{
+
+	}
+}
---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+	public void StartGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("baseScene");
+	}
+
+	public void OpenHelp()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Help");
+	}
+
+	public void OpenMain()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Menu");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+}
